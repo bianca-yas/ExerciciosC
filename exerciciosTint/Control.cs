1ed63120dd0f348a93b434b40704e9927ad89ab4
BIANCA YASMIM SILVA DE ASSIS <bianca.ysassis@senacsp.edu.br>
2025-03-26 17:15:41 -0300

    segundo commit

--- exerciciosTint/Control.cs
+++ exerciciosTint/Control.cs
@@ -20,7 +20,17 @@ namespace exerciciosTint
             Console.WriteLine("\n\nEscolha uma das opções abaixo: " +
                               "\n0. Sair" +
                               "\n1. Mostrar Números" +
-                              "\n2. Média de três notas");
+                              "\n2. Média de três notas" +
+                              "\n3. Verificador" +
+                              "\n4. Tabuada" +
+                              "\n5. Fatorial" +
+                              "\n6. Potencia" +
+                              "\n7. Raiz" +
+                              "\n8. Ímpar ou Par" +
+                              "\n9. Ordem Crescente" +
+                              "\n10. Contar Vogais" +
+                              "\n11. Inverter Palavra" +
+                              "\n12. Primos");
 
         }//fim do método
 
@@ -67,6 +77,87 @@ namespace exerciciosTint
                         {
                             Console.WriteLine("\n\nA média é: " + media);
                         }
+                        break;
+                    case 3:
+                        Console.WriteLine("Informe um número: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.verificador(num));
+                        break;
+                    case 4:
+                        Console.WriteLine("Informe um número");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        if(num < 0)
+                        {
+                            Console.WriteLine("Erro! Digite um número positivo");
+                        }
+                        Console.WriteLine(this.exer.tabuada(num));
+                        break;
+                    case 5:
+                        Console.WriteLine("Informe um número: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        if(num < 0)
+                        {
+                            Console.WriteLine("Informe um número positivo");
+                        }
+                        Console.WriteLine(this.exer.fatorial(num));
+                        break;
+                    case 6:
+                        Console.WriteLine("Informe um número: ");
+                        num1 = (int)Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe o número que vai ser elevado: ");
+                        num2 = (int)Convert.ToDouble(Console.ReadLine());
+                        if((num1 <= 0) || (num2 <= 0))
+                        {
+                            Console.WriteLine("Erro! Digite um número positivo.");
+                        }
+                        Console.WriteLine(this.exer.potencia(num1,num2));
+                        break;
+                    case 7:
+                        Console.WriteLine("Informe um número: ");
+                        num1 = (int)Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Informe o segundo número: ");
+                        num2 = (int)Convert.ToDouble(Console.ReadLine());
+                        if((num1 <= 0) || (num2 <= 0))
+                        {
+                            Console.WriteLine("Informe um número positivo");
+                        }
+                        Console.WriteLine(this.exer.raiz(num1,num2));
+                        break;
+                    case 8:
+                        Console.WriteLine("Informe um número: ");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        if(num <= 0)
+                        {
+                            Console.WriteLine("Informe um número positivo");
+                        }
+                        Console.WriteLine(this.exer.imparPar(num));
+                        break;
+                    case 9:
+                        Console.WriteLine("Informe um número: ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Informe o segundo número: ");
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                           
+                        Console.WriteLine(this.exer.ordemC(num1,num2));
+                        break;
+                    case 10:
+                        Console.WriteLine("Escreva um texto: ");
+                        string vogais = Console.ReadLine();
+
+                        Console.WriteLine(this.exer.vogaisContar(vogais));
+                        break;
+                    case 11:
+                        Console.WriteLine("Escreva um texto");
+                        string palavra = Console.ReadLine();
+
+                        Console.WriteLine(this.exer.inverter(palavra));
+                        break;
+                    case 12:
+                        Console.WriteLine("Informe um número");
+                        num = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine(this.exer.primos(num));
                         break;
                     default:
                         Console.WriteLine("Opção escolhida não é válida!");
--- exerciciosTint/ExerciciosIniciais.cs
+++ exerciciosTint/ExerciciosIniciais.cs
@@ -12,12 +12,12 @@ namespace exerciciosTint
         private int num1;
         private int num2;
 
-        public ExerciciosIniciais() 
+        public ExerciciosIniciais()
         {
 
         }//fim do construtor
 
-        public ExerciciosIniciais(int num1, int num2) 
+        public ExerciciosIniciais(int num1, int num2)
         {
             this.num1 = num1;
             this.num2 = num2;
@@ -32,15 +32,15 @@ namespace exerciciosTint
 
         public string imprimir()
         {
-            return "\nPrimeiro Número: "+ getNum1() +
+            return "\nPrimeiro Número: " + getNum1() +
                    "\nSegundo Número: " + getNum2();
         }//fim do imprimir
 
-        public double media(double nota1,double nota2,double nota3)
+        public double media(double nota1, double nota2, double nota3)
         {
             double med = 0;
-            if((nota1 >= 0 && nota1 <= 10) 
-              && (nota2 >= 0 && nota2 <= 10) 
+            if ((nota1 >= 0 && nota1 <= 10)
+              && (nota2 >= 0 && nota2 <= 10)
               && (nota3 >= 0 && nota3 <= 10))
             {
                 med = (nota1 + nota2 + nota3) / 3;
@@ -49,5 +49,118 @@ namespace exerciciosTint
             return -1;
         }
 
-    }//fim da classe
+        public string verificador(int num)
+        {
+            if (num < 0)
+            {
+                return "Esse número é negativo.\n";
+            } else if (num > 0)
+            {
+                return "Esse número é positivo.\n";
+            }
+            else
+            {
+                return "Esse número é Zero.\n";
+            }
+        } //fim do verificador
+
+        public string tabuada(int num)
+        {
+            string resultado = "";
+
+            for (int i = 0; i <= 10; i++)
+            {
+                resultado += "\n" + (num) + " * " + (i) + " = " + (num * i);
+            }
+            return resultado;
+        } //fim da tabuada
+
+        public int fatorial(int num)
+        {
+            for (int i = num - 1; i >= 1; i--)
+            {
+                num *= i;
+            }
+            return num;
+        }//fim do fatorial
+
+        public double potencia(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }//fim da potencia
+
+        public double raiz(double num1, double num2)
+        {
+            return Math.Sqrt(num1) + Math.Sqrt(num2);
+        }//fim da raiz
+
+        public string imparPar(int num)
+        {
+            string resultado = "";
+            if (num / 2 == 0)
+            {
+                resultado = "Esse número é par";
+            }
+            else
+            {
+                resultado = "Esse número é ímpar";
+            }
+            return resultado;
+        }//fim do impar par
+
+        public string ordemC(int num1, int num2)
+        {
+            string ordem = "";
+            if (num1 > num2)
+            {
+                ordem = "A ordem dos números é: " + num1 + "\n" + num2;
+            } else if (num2 > num1)
+            {
+                ordem = "A ordem dos números é: " + num2 + "\n" + num1;
+            }
+            return ordem;
+        }
+
+
+        public string vogaisContar(string vogais)
+        {
+            vogais = vogais.ToLower();
+            int contarVogais = 0;
+            string vogal = "aeiou";
+
+            foreach(char c in vogais)
+            {
+                if (vogal.Contains(c))
+                {
+                    contarVogais++;
+                }
+            }
+            return "Essa palavra tem " + contarVogais + " vogais";
+        }
+
+        public string inverter(string palavra)
+        {
+            char[] caracteres = palavra.ToCharArray();
+
+            Array.Reverse(caracteres);
+            string invertida = new string(caracteres);
+            return invertida;
+        }
+
+        public string primos(int num)
+        {
+            if(num <= 1)
+            {
+                return "Não é primo";
+            }
+            for(int i = 2; i <= Math.Sqrt(num);i++)
+            {
+                if(num % i == 0)
+                {
+                    return "Não é primo";
+                }
+            }
+            return "É primo";
+        }
+  }//fim da classe
 }//fim do projeto
